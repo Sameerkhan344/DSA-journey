a1df53a35ca55df288dd49db35a0a67d548b73ee
Sameerkhan344 <khansame34@gmail.com>
2026-06-26 10:50:32 +0530

    update: is Palindrome Number

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 // count the digit 
 
-function countDigit(n) {
-    if (n == 0) return 1;
-    let count = 0;
-    n = Math.abs(n)
+// function countDigit(n) {
+//     if (n == 0) return 1;
+//     let count = 0;
+//     n = Math.abs(n)
+//     while (n > 0) {
+//         n = Math.floor(n / 10);
+//         count++;
+//     }
+//     return count++;
+// }
+// let num = 123;
+// const result = countDigit(num);
+// console.log(result);
+
+//palindrome number
+function isPalindrome(n) {
+    if (n < 0) return false;
+    let xCopy = n
+    let rev = 0;
+
     while (n > 0) {
+        let rem = n % 10;
+        rev = (rev*10) + rem;
         n = Math.floor(n / 10);
-        count++;
     }
-    return count++;
+    return rev === xCopy;
 }
-let num = 123;
-const result = countDigit(num);
-console.log(result);
+let num = 121;
+const result = isPalindrome(num);
+console.log(result)
+
