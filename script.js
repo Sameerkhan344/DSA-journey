a03ab665dd9f65f2e1db7ba68a56aa4f7206224d
Sameerkhan344 <khansame34@gmail.com>
2026-06-26 10:52:02 +0530

    update: reverse Integers programe

--- script.js
+++ script.js
@@ -15,19 +15,33 @@
 // console.log(result);
 
 //palindrome number
-function isPalindrome(n) {
-    if (n < 0) return false;
-    let xCopy = n
-    let rev = 0;
+// function isPalindrome(n) {
+//     if (n < 0) return false;
+//     let xCopy = n
+//     let rev = 0;
 
+//     while (n > 0) {
+//         let rem = n % 10;
+//         rev = (rev*10) + rem;
+//         n = Math.floor(n / 10);
+//     }
+//     return rev === xCopy;
+// }
+// let num = 121;
+// const result = isPalindrome(num);
+// console.log(result)
+
+//reverse the Integer
+
+function reverseInteger(n) {
+    let rev = 0;
     while (n > 0) {
         let rem = n % 10;
-        rev = (rev*10) + rem;
+        rev = (rev * 10) + rem;
         n = Math.floor(n / 10);
     }
-    return rev === xCopy;
+    return rev;
 }
-let num = 121;
-const result = isPalindrome(num);
+let num = 123456852;
+const result = reverseInteger(num);
 console.log(result)
-
