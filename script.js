fc4ab676367bad5d859068b6b279a2a3c07a579a
Sameerkhan344 <khansame34@gmail.com>
2026-06-27 14:22:15 +0530

    update: Remove duplicate from an array using two pointer approch

--- script.js
+++ script.js
@@ -33,15 +33,33 @@
 
 //reverse the Integer
 
-function reverseInteger(n) {
-    let rev = 0;
-    while (n > 0) {
-        let rem = n % 10;
-        rev = (rev * 10) + rem;
-        n = Math.floor(n / 10);
+// function reverseInteger(n) {
+//     let rev = 0;
+//     while (n > 0) {
+//         let rem = n % 10;
+//         rev = (rev * 10) + rem;
+//         n = Math.floor(n / 10);
+//     }
+//     return rev;
+// }
+// let num = 123456852;
+// const result = reverseInteger(num);
+// console.log(result)
+
+//Remove duplicate without taking new new array using two pointers-
+
+function removeDuplicate(nums) {
+    if (nums.length === 0) return [];
+    let x = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== nums[x]) {
+            x++;
+            nums[x] = nums[i]
+        }
     }
-    return rev;
+    return nums.slice(0, x + 1);
+
 }
-let num = 123456852;
-const result = reverseInteger(num);
+let arr = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5];
+const result = removeDuplicate(arr);
 console.log(result)
